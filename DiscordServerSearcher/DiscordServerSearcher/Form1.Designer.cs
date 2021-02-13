03a6e47531474e49f44d54a4dadcc952adb8355c
Artem <74727214+VantyArtem@users.noreply.github.com>
2021-02-13 13:49:59 +0900

    Add files via upload

---
+++ DiscordServerSearcher/DiscordServerSearcher/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿
+namespace DiscordServerSearcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(135, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 169);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Check";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Has Server:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(193, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "8";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Length:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(195, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "----";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "https://discord.com/invite/",
+            "https://discord.com/gifts/",
+            "https://discord.gg/",
+            "https://discord.gift/"});
+            this.comboBox1.Location = new System.Drawing.Point(43, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(192, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.Text = "https://discord.com/invite/";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 40);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(52, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Digits";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(69, 40);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(85, 17);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Lower Chars";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(186, 115);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(47, 20);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "1000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(133, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Delay [ms]";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 198);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Check BlackOut";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(135, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Has BlackOut:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(210, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "----";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "URL:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 70);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Type:";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(76, 114);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(53, 20);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Stop";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 97);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(69, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Auto Search:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(82, 97);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Stoped";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(160, 40);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(85, 17);
+            this.checkBox3.TabIndex = 22;
+            this.checkBox3.Text = "Upper Chars";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 234);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Discord Server Searcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ DiscordServerSearcher/DiscordServerSearcher/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DiscordServerSearcher
+{
+    public partial class Form1 : Form
+    {
+        void Open(string URL)
+        {
+            const string PathToFile = @"C:\Program Files (x86)\Opera\launcher.exe";
+
+            Process.Start(PathToFile, $"--ran-launcher --remote {URL}");
+        }
+
+        string GenerateString(int Length,bool Digits,bool L,bool U)
+        {
+            const string digits = "0123456789";
+
+            const string l = "abcdefghijklmnopqrstuvwxyz";
+
+            const string u = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            string abc = "";
+
+            if (Digits)
+            {
+                abc += digits;
+            }
+
+            if (L)
+            {
+                abc += l;
+            }
+
+            if (U)
+            {
+                abc += u;
+            }
+
+            Random random = new Random();
+
+            string Result = "";
+
+            for (int i = 0; i < Length; i++)
+            {
+                Result += abc[random.Next(abc.Length)];        
+            }
+
+            return Result;
+        }
+
+        bool HasServer(string URL)
+        {
+            WebClient client = new WebClient();
+            string HTML = client.DownloadString(URL);
+            return !HTML.Contains($@"<title>Discord</title>");
+        }
+
+        public Thread thread = new Thread(()=> { });
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Auto Search Start
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label10.Text == "Stoped")
+            {
+                label10.ForeColor = Color.Green;
+                label10.Text = "Working";
+                thread = new Thread(() =>
+                {
+                    bool Has = false;
+                    while (Has != true)
+                    {
+                        if (checkBox1.Checked || checkBox2.Checked || checkBox3.Checked)
+                        {
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                textBox1.Text = comboBox1.Text + GenerateString(int.Parse(textBox2.Text), checkBox1.Checked, checkBox2.Checked, checkBox3.Checked);
+                            });
+                        }
+                        else
+                        {
+                            MessageBox.Show("CheckBoxes can't be empty", "Err0r", MessageBoxButtons.OK);
+                            break;
+                        }
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            label3.Text = "";
+                        });
+                        if (textBox1.Text.Length != 0)
+                        {
+                            Has = HasServer(textBox1.Text);
+                            if (Has)
+                            {
+                                label3.ForeColor = Color.Green;
+                                this.Invoke((MethodInvoker)delegate
+                                {
+                                    label3.Text = "True";
+                                });
+                            }
+                            else
+                            {
+                                label3.ForeColor = Color.Red;
+                                this.Invoke((MethodInvoker)delegate
+                                {
+                                    label3.Text = "False";
+                                });
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Url can't be empty", "Err0r", MessageBoxButtons.OK);
+                            break;
+                        }
+                        if (textBox3.Text != "0")
+                        {
+                            Thread.Sleep(int.Parse(textBox3.Text));
+                        }
+                    }
+                    if (Has)
+                    {
+                        MessageBox.Show("Server is Find!", "Congratulations!", MessageBoxButtons.OK);
+                        label10.ForeColor = Color.Red;
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            label10.Text = "Stoped";
+                        });
+                    }
+                    else
+                    {
+                        label10.ForeColor = Color.Red;
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            label10.Text = "Stoped";
+                        });
+                    }
+                });
+                thread.Start();
+            }
+        }
+        //Generate
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked || checkBox2.Checked || checkBox3.Checked)
+            {
+                textBox1.Text = comboBox1.Text + GenerateString(int.Parse(textBox2.Text), checkBox1.Checked, checkBox2.Checked, checkBox3.Checked);
+            }
+            else
+            {
+                MessageBox.Show("CheckBoxes can't be empty", "Err0r", MessageBoxButtons.OK);
+            }
+        }
+        //Open
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 0 && comboBox1.Text.Length != 0)
+            {
+                Open(textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("Url or Type can't be empty", "Err0r", MessageBoxButtons.OK);
+            }
+        }
+        //Check
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 0)
+            {
+                label3.Text = "";
+                bool Has = HasServer(textBox1.Text);
+                if (Has)
+                {
+                    label3.ForeColor = Color.Green;
+                    label3.Text = "True";
+                }
+                else
+                {
+                    label3.ForeColor = Color.Red;
+                    label3.Text = "False";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Url can't be empty", "Err0r", MessageBoxButtons.OK);
+            }
+        }
+        //Check BlackOut
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string ForeverURL = "https://discord.com/invite/lol";
+            label6.Text = "";
+            bool Has = HasServer(ForeverURL);
+            if (!Has)
+            {
+                label6.ForeColor = Color.Red;
+                label6.Text = "True";
+            }
+            else
+            {
+                label6.ForeColor = Color.Green;
+                label6.Text = "False";
+            }
+        }
+        //Auto Search Stop
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (label10.Text == "Working")
+            {
+                label10.ForeColor = Color.Red;
+                label10.Text = "Stoped";
+                thread.Abort();
+            }
+        }
+    }
+}
